6f568643e78c3c3efe8d218214c8853172cc92a4
Sergey Donyk <doncarlos@i.ua>
2023-06-29 00:49:40 +0300

    addPost

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,19 @@
 const express = require('express')
-const { listContacts, getContactById } = require('../../models/contacts')
+const { listContacts, getContactById, addContact } = require('../../models/contacts')
+const Joi = require("joi")
+const addSchema = Joi.object({
+  name: Joi.string().required().messages({
+    'any.required': 'Missing required name field',
+  }),
+  email: Joi.string().required().messages({
+    'any.required': 'Missing required email field',
+  }),
+  phone: Joi.string().required().pattern(/^\(\d{3}\) \d{3}-\d{4}$/).messages({
+    'any.required': 'Missing required phone field',
+    'string.pattern.base': 'Invalid phone number format. Expected format: (XXX) XXX-XXXX',
+  }),
+});
+
 
 const router = express.Router()
 router.get('/', async (req, res, next) => {
@@ -15,7 +29,7 @@ router.get('/:id', async (req, res, next) => {
   try {
     const contactId = req.params.id;
     const contact = await getContactById(contactId);
-    console.log(contact);
+    // console.log(contact);
     if (contact) {
       res.status(200).json(contact);
     } else {
@@ -27,10 +41,20 @@ router.get('/:id', async (req, res, next) => {
 });
 
 
+router.post('/', async (req, res) => {
+  const { name, email, phone } = req.body;
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  const { error } = addSchema.validate({ name, email, phone }, { abortEarly: false });
+  if (error) {
+    const errorMessage = error.details.map(detail => detail.message).join(', ');
+    return res.status(400).json({ message: errorMessage });
+  }
+
+  const newContact = await addContact({ name, email, phone });
+
+  return res.status(201).json({ id: newContact.id, name, email, phone });
+});
+
 
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
